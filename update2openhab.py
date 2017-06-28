8154b4d39fc36f7835ab26c8b7711dcca9c2c3c4
svensommer <klaas.klaar@googlemail.com>
2017-06-28 10:54:17 +0200

    added try catch block for uploading the data

--- update2openhab.py
+++ update2openhab.py
@@ -7,9 +7,9 @@ import datetime
 
 openhab_host = 'openhabianpi'
 openhab_port = '8080'
-
-mac_addresses = ['C4:7C:8D:62:E6:B7','C4:7C:8D:62:E6:A8']
-flower_names = ['Lavendel','Olivenbaum']
+#defekt 'C4:7C:8D:62:E6:A8'
+mac_addresses = ['C4:7C:8D:62:E6:B7','C4:7C:8D:63:64:44','C4:7C:8D:63:74:E8','C4:7C:8D:63:35:BA','C4:7C:8D:63:7B:BF',]
+flower_names = ['Lavendel','Olivenbaum','TomTato','Sommermargerite','Maennertreu']
 
 def send_status_to_openhab(flower, key, value):
     url = 'http://{host}:{port}/rest/items/MiFlora_{flower}_{key}/state'.format(host=openhab_host, port=openhab_port, flower=flower, key=key)
@@ -28,12 +28,16 @@ def send_status_to_openhab(flower, key, value):
             print(datetime.datetime.now(), e)
 
 for index in range(len(mac_addresses)):
-    poller = MiFloraPoller(mac_addresses[index])
-    print(datetime.datetime.now(), "Getting data from Mi Flora '" , flower_names[index],"'" )
-    send_status_to_openhab(flower=flower_names[index],key='Firmware', value=poller.firmware_version())
-    send_status_to_openhab(flower=flower_names[index],key='Temperature', value=poller.parameter_value("temperature"))
-    send_status_to_openhab(flower=flower_names[index],key='Moisture', value=poller.parameter_value(MI_MOISTURE))
-    send_status_to_openhab(flower=flower_names[index],key='Light', value=poller.parameter_value(MI_LIGHT))
-    send_status_to_openhab(flower=flower_names[index],key='Conductivity', value=poller.parameter_value(MI_CONDUCTIVITY))
-    send_status_to_openhab(flower=flower_names[index],key='Battery', value=poller.parameter_value(MI_BATTERY))
-    print(datetime.datetime.now(), "Finished!")
+    try:
+        poller = MiFloraPoller(mac_addresses[index])
+        print(datetime.datetime.now(), "Getting data from Mi Flora '" , flower_names[index],"'" )
+        send_status_to_openhab(flower=flower_names[index],key='Firmware', value=poller.firmware_version())
+        send_status_to_openhab(flower=flower_names[index],key='Temperature', value=poller.parameter_value("temperature"))
+        send_status_to_openhab(flower=flower_names[index],key='Moisture', value=poller.parameter_value(MI_MOISTURE))
+        send_status_to_openhab(flower=flower_names[index],key='Light', value=poller.parameter_value(MI_LIGHT))
+        send_status_to_openhab(flower=flower_names[index],key='Conductivity', value=poller.parameter_value(MI_CONDUCTIVITY))
+        send_status_to_openhab(flower=flower_names[index],key='Battery', value=poller.parameter_value(MI_BATTERY))
+        print(datetime.datetime.now(), "Finished!")
+    except Exception as e:
+        print(datetime.datetime.now(), "-Error-")
+        print(datetime.datetime.now(), e)
